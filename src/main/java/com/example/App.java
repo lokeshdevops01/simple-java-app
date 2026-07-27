f2471c0dd72f29ddfaefe142361714adc7e56ae6
lokeshdevops01 <mankarsl28@gmail.com>
2026-07-27 16:50:29 +0530

    updated app.java fire

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,33 @@
 package com.example;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to My Simple Java Application!");
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(8080), 0
+        );
+
+        server.createContext("/", (HttpExchange exchange) -> {
+
+            String response = "Welcome to My Simple Java Application!";
+
+            exchange.sendResponseHeaders(200, response.length());
+
+            try (OutputStream outputStream = exchange.getResponseBody()) {
+                outputStream.write(response.getBytes());
+            }
+        });
+
+        server.start();
+
+        System.out.println("Application started on port 8080");
     }
 }
-
